77ef67053fdfc951c4177465406940746cdf2420
maciek-slon <maciek.slon@gmail.com>
2016-05-10 13:59:48 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <rapp/objects/picture/picture.hpp>
+#include <rapp-robots-api/vision/vision.hpp>
+
+int main() {
+	rapp::robot::vision vis;
+	std::cout << "Hello world!\n";
+	return 0;
+}
